481285c16b9e040fc16a81691501f01fbdf446ec
Flokki217 <flokki217@gmail.com>
2023-07-06 19:16:00 +0200

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galeryListEl = document.querySelector(".gallery");
+
+const imageArr = galleryItems
+  .map((image) => {
+    return `<li class = "gallery__item" > <a  href= "${image.original}" class="gallery__link" ><img class = "gallery__image" src = "${image.preview}" data-source="${image.original}" alt = "${image.description}"></a></li>`;
+  })
+  .join("");
+galeryListEl.insertAdjacentHTML("beforeend", imageArr);
+const galeryItemEl = document.querySelector(".gallery__item");
+galeryListEl.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  blockStandartAction(evt);
+  if (evt.target.tagName !== "IMG") {
+    return;
+  }
+
+  const instance =
+    basicLightbox.create(` <img src = "${evt.target.dataset.source}" width = "800" height="600">
+`);
+  instance.show();
+
+  galeryListEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
