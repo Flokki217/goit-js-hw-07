e54ac729ff07b08473922cec5847115a506d2da8
Flokki217 <flokki217@gmail.com>
2023-07-07 10:07:40 +0200

    I am ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const imageList = galleryItems
+  .map((image) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`;
+  })
+  .join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", imageList);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
